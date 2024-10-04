03951f9d8ad3cc6924041de1df75ccbc76f50fad
Samuel Oluyori <samueloluyori149@gmail.com>
2024-10-04 15:37:19 +0100

    nav response

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,17 +13,12 @@ const transition = {
   restSpeed: 0.001,
 };
 
-export const MenuItem = ({
-  setActive,
-  active,
-  item,
-  children
-}) => {
+export const MenuItem = ({ setActive, active, item, children }) => {
   return (
-    <div onMouseEnter={() => setActive(item)} className="relative ">
+    <div onMouseEnter={() => setActive(item)} className="relative">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white">
+        className="cursor-pointer text-black hover:opacity-90 dark:text-white text-base md:text-lg">
         {item}
       </motion.p>
       {active !== null && (
@@ -32,15 +27,12 @@ export const MenuItem = ({
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={transition}>
           {active === item && (
-            <div
-              className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+            <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4 w-full max-w-xs md:max-w-md lg:max-w-lg">
               <motion.div
                 transition={transition}
                 layoutId="active"
                 className="bg-white dark:bg-white backdrop-blur-sm rounded-2xl overflow-hidden border border-green/[0.2] dark:border-white/[0.2] shadow-xl">
-                <motion.div
-                  layout
-                  className="w-max h-full p-4">
+                <motion.div layout className="p-4">
                   {children}
                 </motion.div>
               </motion.div>
@@ -52,38 +44,30 @@ export const MenuItem = ({
   );
 };
 
-export const Menu = ({
-  setActive,
-  children
-}) => {
+export const Menu = ({ setActive, children }) => {
   return (
     <nav
       onMouseLeave={() => setActive(null)}
-      className="relative rounded-full border border-transparent bg-green-700 shadow-input flex justify-center space-x-4 px-8 py-6">
+      className="relative rounded-full border border-transparent bg-green-700 shadow-input flex justify-center space-x-4 px-4 md:px-8 py-4 md:py-6">
       {children}
     </nav>
   );
 };
 
-export const ProductItem = ({
-  title,
-  description,
-  href,
-  src
-}) => {
+export const ProductItem = ({ title, description, href, src }) => {
   return (
-    <Link href={href} className="flex space-x-2">
+    <Link href={href} className="flex space-x-2 items-center">
       <Image
         src={src}
         width={140}
         height={70}
         alt={title}
         className="flex-shrink-0 rounded-md shadow-2xl" />
-      <div>
+      <div className="flex flex-col">
         <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
           {title}
         </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-green-400">
+        <p className="text-neutral-700 text-sm max-w-full md:max-w-[10rem] dark:text-green-400">
           {description}
         </p>
       </div>
@@ -91,14 +75,11 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({
-  children,
-  ...rest
-}) => {
+export const HoveredLink = ({ children, ...rest }) => {
   return (
     <Link
       {...rest}
-      className="text-green-400 dark:text-green-500 hover:text-green-800 ">
+      className="text-green-400 dark:text-green-500 hover:text-green-800">
       {children}
     </Link>
   );
